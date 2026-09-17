68726c035946d09b1064b150592da0783ebf8e34
Echo-com123 <2945770462@qq.com>
2026-09-17 12:16:39 +0800

    改进

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import Demo01 from './components/Demo01.vue'
-import Demo08 from './components/Demo08.vue'
+import Demo07 from './components/Demo07.vue'
 </script>
 
 <template>
-  <Demo08 />
+  <Demo07 />
 </template>
